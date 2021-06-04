f315097ba65c96fd7668fbd455d5e6a6914f9118
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2021-06-03 23:18:29 -0300

    Estudos sobre Navegação
    Criação dos componentes para o estudo com navegação

---
+++ navegacao/src/components/layout/Content.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Content.css'
+const Content = props =>(
+    <aside className='Content'>
+        <h1>Content Componente</h1>
+    </aside>
+)
+export default Content
---
+++ navegacao/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './index.css'
+import App from './views/App'
+ReactDOM.render(
+    <div>
+        <App></App>
+    </div>
+    
+    ,document.getElementById('root')
+) 
