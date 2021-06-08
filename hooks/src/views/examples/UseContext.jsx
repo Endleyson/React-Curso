97c86bb35f1259221e51dff1e7f1e10eb2bdffb6
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2021-06-08 13:33:53 -0300

    useReducer
    princípios do useReduce

--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -1,19 +1,36 @@
 import React, {useContext} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 import DataContext from '../../data/DataContext'
+import {AppContext} from '../../data/Store'
 
 const UseContext = (props) => {
     const context = useContext(DataContext)
+    const {number, text, setNumber} = useContext(AppContext)
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+            <SectionTitle title="Exercicio #01"/>
             <div className="center">
                 <span className="text">{context.state.text}</span>
                 <span className="text">{context.state.number}</span>
-                <span className="text red">{context.state.bio}</span>
+                <span className="red">{context.state.bio}</span>
+            </div>
+            <SectionTitle title="Exercicio #02" />
+            <div className="center">
+            <span className="text">{text}</span>
+            <span className="text">{number}</span>
+                <div>
+                    <button className="btn"
+                    onClick={()=> setNumber(number - 1) }>
+                        -1</button>
+                    <button className="btn"
+                    onClick={()=> setNumber(number + 1) }>
+                        +1</button>
+                </div>
             </div>
         </div>
     )
