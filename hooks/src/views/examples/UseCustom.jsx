10f3dcd581e6eaa9ad612d1afe0d3f1485f6613a
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2021-06-09 16:47:05 -0300

    useCustom
    atualização da segunda parte do useCustom

--- hooks/src/views/examples/UseCustom.jsx
+++ hooks/src/views/examples/UseCustom.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import {useCounter} from '../../Hook/UseCounter'
+import { useFetch } from '../../Hook/useFetch'
 
 const UseRef = (props) => {
+    const[count,inc,dec]= useCounter()
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+    const response = useFetch(url)
+    function showStates(states){
+        return states.map(state => <li>{state.nome} - {state.sigla}</li>)
+    }
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+            <SectionTitle title="Exercicio #01"/>
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn" onClick={()=>inc()}>+10</button>
+                    <button className="btn" onClick={()=>dec()}>-10</button>
+                </div>               
+            </div>
+            <SectionTitle title="Exercicio #02"/>
+                <div className="center">
+                    <ul>
+                        {showStates ? (response.data) : false}
+                    </ul>
+                </div>
+            
         </div>
     )
 }
