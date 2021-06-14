d0e4c742a06dace5ddcf796d099412ab0d2a585a
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2021-06-14 13:17:12 -0300

    up

--- hooks/src/views/examples/NotFound.jsx
+++ hooks/src/views/examples/NotFound.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-
+import Logo from '../../image/404error.png'
 const Home = props => (
     <div className="Home">
         <PageTitle error
             title="ERROR 404"
             subtitle="Opss... Página Não Encontrada!" />
+            <img src={Logo} alt="logo"/>
+   
     </div>
 )
 
